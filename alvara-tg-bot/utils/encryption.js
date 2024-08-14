c5b9c1f9bfbe0f1a490e1a672ec3abdb2faa54dc
kamalbuilds <geniusamansingh@gmail.com>
2024-08-15 04:28:00 +0530

    fix a bunch of errors

--- alvara-tg-bot/utils/encryption.js
+++ alvara-tg-bot/utils/encryption.js
@@ -1,11 +1,11 @@
 const crypto = require("crypto-js");
 
 function encrypt(text) {
-  return crypto.AES.encrypt(text, process.env.TG_WALLET_BOT_TOKEN).toString();
+  return crypto.AES.encrypt(text, process.env.TELEGRAM_BOT_TOKEN).toString();
 }
 
 function decrypt(cipherText) {
-  const bytes = crypto.AES.decrypt(cipherText, process.env.TG_WALLET_BOT_TOKEN);
+  const bytes = crypto.AES.decrypt(cipherText, process.env.TELEGRAM_BOT_TOKEN);
   return bytes.toString(crypto.enc.Utf8);
 }
 
