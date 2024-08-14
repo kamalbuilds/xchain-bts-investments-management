5525ed75482356f192fe8cf7f31bb372529fb6cc
Kamal Nayan <geniusamansingh@gmail.com>
2024-08-14 12:46:32 +0000

    setup the alvara tg bot

---
+++ alvara-tg-bot/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+const URI = `/webhook/${BOT_TOKEN}`;
+
+
+const WEBHOOK_URL =
+  "https://pinggy" +
+  URI;
+
+app.use(bodyParser.json());
+
+const init = async () => {
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
+
+app.post(URI, async (req, res) => {
+  console.log(req.body);
+
+  const chatId = req.body.message.chat.id;
+  const text = req.body.message.text;
+
+  // Respond to the message
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: `You said: ${text}`,
+  });
+
+  return res.send();
+});
+
+app.listen(port, async () => {
+  console.log(`Telegram bot listening on port ${port}`);
+  await init(); // Set the webhook
+});
